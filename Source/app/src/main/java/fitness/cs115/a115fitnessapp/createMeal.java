489961b19c7314bd1acd2dd8fe8fc9ac5b53e8fb
mdeyell <mdeyell@ucsc.du>
2016-10-09 13:19:40 -0700

    Locked existing activities to portrait mode.

--- Source/app/src/main/java/fitness/cs115/a115fitnessapp/createMeal.java
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/createMeal.java
@@ -1,7 +1,10 @@
 package fitness.cs115.a115fitnessapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Matthew on 10/9/16.
@@ -12,5 +15,15 @@ public class createMeal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_meal);
+
+
+        Button addFood = (Button) findViewById(R.id.addFood);
+        addFood.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(createMeal.this, addItemToMeal.class);
+                startActivity(intent);
+            }
+        });
     }
 }
