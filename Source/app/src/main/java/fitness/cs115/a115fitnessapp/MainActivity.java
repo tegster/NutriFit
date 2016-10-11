cb4b6ed429c7cc1c1046be97b1a3ec5409a6908c
Henry Pan <henry-pan@users.noreply.github.com>
2016-10-10 22:32:35 -0700

    reorganization 1

--- Source/app/src/main/java/fitness/cs115/a115fitnessapp/MainActivity.java
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/MainActivity.java
@@ -2,8 +2,6 @@ package fitness.cs115.a115fitnessapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -20,14 +18,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         Button button = (Button) findViewById(R.id.meal);
         button.setOnClickListener(new Button.OnClickListener() {
@@ -42,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, nutritionOverview.class);
+                startActivity(intent);
+            }
+        });
+
+        Button program_list = (Button) findViewById(R.id.proglist);
+        program_list.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, programList.class);
                 startActivity(intent);
             }
         });
---
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/programList.java
@@ -0,0 +1,18 @@
+package fitness.cs115.a115fitnessapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Henry on 10/10/2016.
+ */
+
+public class programList extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_program_list);
+
+    }
+}
