0b80a44b605ed259f7320915ffe92b51996133ca
mdeyell <mdeyell@ucsc.du>
2016-10-13 13:00:14 -0700

    added edit meal activity

--- Source/app/src/main/java/fitness/cs115/a115fitnessapp/meal_createMeal.java
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/meal_createMeal.java
@@ -20,6 +20,13 @@ public class meal_createMeal extends AppCompatActivity {
         //actual meal time
         //food items
             //includes name+ calories
-        
+
+        //flow
+        //1. enter meal name (which will be the table name)
+        //2.    check if meal name is has already been used or not (display error message and make user pick new name if it has)
+        //3. create table
+        //4. launch edit meal activity with the meal name being passed in
+        //5. have button for add item to meal
+        //6.
     }
 }
