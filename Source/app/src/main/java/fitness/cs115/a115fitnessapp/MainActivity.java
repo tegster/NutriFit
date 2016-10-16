0748351848f7ab6d07220dd4ee484adf1734d1ac
Henry Pan <henry-pan@users.noreply.github.com>
2016-10-16 03:34:08 -0700

    temporary colors

--- Source/app/src/main/java/fitness/cs115/a115fitnessapp/MainActivity.java
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
         Button nutr_overview_btn = (Button) findViewById(R.id.nutr_overview);
         nutr_overview_btn.setOnClickListener(new Button.OnClickListener() {
