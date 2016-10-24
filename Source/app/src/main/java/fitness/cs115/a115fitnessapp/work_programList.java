6a7fd01bcf66bb628ce5f43b90f392febfa66ae1
Henry Pan <henry-pan@users.noreply.github.com>
2016-10-23 22:46:42 -0700

    added tracker initial

--- Source/app/src/main/java/fitness/cs115/a115fitnessapp/work_programList.java
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/work_programList.java
@@ -28,7 +28,7 @@ public class work_programList extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_program_list);
 
-        work_db= new work_DBHelper(this);
+        work_db = new work_DBHelper(this);
         work_db.reset_default_values();
         //check for user programs. If there aren't any, bring up a popup menu prompting users
         //to choose "Create New" or a pre-included beginner program. Replace with database programs.
---
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/work_tracker.java
@@ -0,0 +1,31 @@
+package fitness.cs115.a115fitnessapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by Henry on 10/23/2016.
+ */
+
+public class work_tracker extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_tracker);
+
+
+
+
+
+
+    }
+
+
+}
