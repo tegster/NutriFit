1e6bac488742b9caba7f3d37a9caac9a29617168
mdeyell <mdeyell@ucsc.du>
2016-10-28 12:36:38 -0700

    Added ability to delete meals from database using UI.

--- Source/app/src/main/java/fitness/cs115/a115fitnessapp/meal_viewAllMeals.java
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/meal_viewAllMeals.java
@@ -1,9 +1,12 @@
 package fitness.cs115.a115fitnessapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.annotation.RequiresPermission;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -23,6 +26,9 @@ import static android.R.layout.simple_list_item_1;
 public class meal_viewAllMeals extends AppCompatActivity {
     ListView listView;
     ArrayList<String> arrTblNames = new ArrayList<String>();
+    AlertDialog.Builder programOptionMenu;
+    private String selectedTable;
+    private final boolean DEBUG = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +36,69 @@ public class meal_viewAllMeals extends AppCompatActivity {
         setContentView(R.layout.activity_view_all_meals);
         listView = (ListView) findViewById(R.id.meals);
         listView.setLongClickable(true);
-        //   ArrayAdapter adapter = new ArrayAdapter<String>(this, simple_list_item_1, dummyArray);
-        //  listView.setAdapter(adapter);
 
+
+        //======================================================================================
+        //  Dialog Boxes
+        //======================================================================================
+        // Program Deletion Dialog Box
+        // - A deletion confirmation dialog box.
+        final AlertDialog.Builder programOptionDelete = new AlertDialog.Builder(this);
+        programOptionDelete.setTitle("Delete Program?");
+        programOptionDelete.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int selection_id) {
+                //actually delete the meal here
+                if (DEBUG) {
+                    System.out.println("selectionId confirm delete: " + selection_id); //selection_id is meaningless
+                    System.out.println("name of table to be dropped is: " + selectedTable);
+                }
+                SQLiteDatabase sdb;
+                sdb = openOrCreateDatabase("meal.db", SQLiteDatabase.CREATE_IF_NECESSARY, null);
+                try {
+                    sdb.execSQL("DROP TABLE IF EXISTS " + selectedTable);
+                } catch (Exception e) {
+                    sdb.execSQL("DROP TABLE IF EXISTS " + '[' + selectedTable + ']');
+                }
+                //now need to reload list view
+                viewMealsInDatabase();
+            }
+        });
+        programOptionDelete.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int selection_id) {
+                dialogInterface.cancel();
+            }
+        });
+
+        // Program Options Menu Dialog Box
+        // - Shown when an item in the ListView is long-clicked.
+        final CharSequence programOptionsMenuOptions[] = new CharSequence[]{"Edit", "Delete"};
+        programOptionMenu = new AlertDialog.Builder(this);
+        programOptionMenu.setItems(programOptionsMenuOptions, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int selection_id) {
+                if (selection_id == 0) {
+                    //edit program
+                    /*
+                    Intent intent = new Intent(meal_viewAllMeals.this, work_createWorkout.class);
+                    intent.putExtra();
+                    startActivity(intent);
+                    */
+                } else {
+                    //delete the program
+                    programOptionDelete.show();
+                }
+            }
+        });
+
+        viewMealsInDatabase();
+
+    }
+
+    //displays the meals in the database in a listview
+    private void viewMealsInDatabase() {
+        arrTblNames.clear();
         SQLiteDatabase db = openOrCreateDatabase("meal.db", SQLiteDatabase.CREATE_IF_NECESSARY, null);
 
         Cursor c = db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
@@ -59,7 +125,12 @@ public class meal_viewAllMeals extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
                                            final int position, long arg3) {
-                Toast.makeText(getApplicationContext(), "long click: " + position + " " + arrTblNames.get(position), Toast.LENGTH_LONG).show();
+                programOptionMenu.show();
+                selectedTable = arrTblNames.get(position);
+                if (DEBUG) {
+                    //    Toast.makeText(getApplicationContext(), "long click: " + position + " " + arrTblNames.get(position), Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "long click: " + position + " " + arrTblNames.get(position) + ", " + selectedTable, Toast.LENGTH_LONG).show();
+                }
                 return true;
             }
         });
@@ -68,8 +139,9 @@ public class meal_viewAllMeals extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, final int position,
                                     long id) {
-                Toast.makeText(getApplicationContext(), "click: " + position + " " + arrTblNames.get(position), Toast.LENGTH_LONG).show();
-
+                if (DEBUG) {
+                    Toast.makeText(getApplicationContext(), "click: " + position + " " + arrTblNames.get(position), Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
