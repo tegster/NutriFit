cc517f5e22531bb0d4ad242005cb54572b2b961d
Henry Pan <henry-pan@users.noreply.github.com>
2016-11-10 03:43:38 -0800

    added files

---
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/work_trackerSetDetail.java
@@ -0,0 +1,26 @@
+package fitness.cs115.a115fitnessapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Henry on 10/23/2016.
+ */
+
+public class work_trackerSetDetail extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_work_tracker);
+
+
+        //this is the file that handles actual set logging
+
+
+
+
+    }
+
+
+}
