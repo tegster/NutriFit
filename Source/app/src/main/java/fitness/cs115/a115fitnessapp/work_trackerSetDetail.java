3881e91d159366df95963b5a8d2e664d9c43a9c5
Henry Pan <henry-pan@users.noreply.github.com>
2016-11-14 04:19:10 -0800

    Plate Calc Algorithm and Layouts

--- Source/app/src/main/java/fitness/cs115/a115fitnessapp/work_trackerSetDetail.java
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/work_trackerSetDetail.java
@@ -12,7 +12,7 @@ public class work_trackerSetDetail extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_work_tracker);
+        setContentView(R.layout.activity_work_tracker_setdetail);
 
 
         //this is the file that handles actual set logging
@@ -22,5 +22,27 @@ public class work_trackerSetDetail extends AppCompatActivity{
 
     }
 
+    //Take weight from EditTexts and return a string to be used in a TextView.
+    public String eachSide(int totalWeight, int barWeight){
+        return "On each side: " + String.valueOf((totalWeight - barWeight) / 2.0);
+    }
+
+    //Take weight from EditTexts and return a string to be used in a TextView.
+    //Precondition: totalWeight must be greater than barWeight.
+    public String plateCalculator(int totalWeight, int barWeight) {
+        final double[] plates =  {45, 35, 25, 10, 5, 2.5};
+        final String[] plateStr = {"45, ", "35, ", "25, ", "10, ", "5, ", "2.5"};
+        String platesNeeded = "Put these plates on the bar: ";
+        double weight = (totalWeight - barWeight) / 2.0;
+
+        for(int i = 0; i < plates.length; i++){
+            double currentPlate = plates[i];
+            while(weight - currentPlate >= 0){
+                weight = weight - currentPlate;
+                platesNeeded = platesNeeded + plateStr[i];
+            }
+        }
+        return platesNeeded;
+    }
 
 }
