20e0fedebb73ac3e0e0eb5f49a1d2dab549ae160
Henry Pan <henry-pan@users.noreply.github.com>
2016-11-17 00:40:32 -0800

    progress on the tracker

--- Source/app/src/main/java/fitness/cs115/a115fitnessapp/work_tracker.java
+++ Source/app/src/main/java/fitness/cs115/a115fitnessapp/work_tracker.java
@@ -9,26 +9,44 @@ import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 /**
  * Created by Henry on 10/23/2016.
  */
 
 public class work_tracker extends AppCompatActivity{
+    String exerciseName = "";
+    String sessID = "";
+    String workoutName = "";
+    work_DBHelper work_db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_work_tracker);
 
+        work_db = new work_DBHelper(this);
+
+        //Get parameters that are passed into the tracker.
+        Intent intent = getIntent();
+        sessID = intent.getExtras().getString("sessID");
+        workoutName = intent.getExtras().getString("wName");
+        setTitle(workoutName);
+
+
         //TODO: grab exercises
-        String[] exercises = {};
+        ArrayList<String> userworkAL = work_db.get_exers_from_work(workoutName);
+        String[] exercises = userworkAL.toArray(new String[userworkAL.size()]);
+
+
 
 
         //======================================================================================
         //  ListView
         //======================================================================================
         //Create the list.
-        //TODO: maybe show frequency of the program within the list.
+        //TODO: use the custom adapter to display exercise name, weight, and sets
         //ListAdapter programListAdapter = new work_programList_adapter(this, programs);
         ListAdapter exerciseListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, exercises);
         ListView exerciseListView = (ListView) findViewById(R.id.lv_exerList);
@@ -36,8 +54,11 @@ public class work_tracker extends AppCompatActivity{
         exerciseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                //programName = String.valueOf(parent.getItemAtPosition(position));
-                //OpenProgram(programName);
+                exerciseName = String.valueOf(parent.getItemAtPosition(position));
+                Intent setIntent = new Intent(work_tracker.this, work_trackerSetList.class);
+                setIntent.putExtra("eName", exerciseName);
+                setIntent.putExtra("sessID", sessID);
+                startActivity(setIntent);
 
             }
         });
